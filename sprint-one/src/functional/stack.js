7c489fec5a0a66593d9f7196b60a19435def7b0d
Billy Rukh <billyrukh@gmail.com>
2015-05-13 17:37:42 -0500

    Passed second test for size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,18 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    count++;
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
-    return 0;
+    return count;
   };
 
   return someInstance;
