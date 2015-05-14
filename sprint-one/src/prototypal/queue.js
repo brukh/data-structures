a3a96081811dc23ff5d9ecb7850c73fcb5809a58
Billy Rukh <billyrukh@gmail.com>
2015-05-14 17:18:01 -0500

    Completed tests for prototypal stack & queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.storage = {};
+  newQueue.count = 0;
+  newQueue.head = 0;
+  newQueue.tail = 0;
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[this.tail] = value;
+  this.count++;
+  this.tail++;
+};
+
+queueMethods.dequeue = function(){
+  if (this.count > 0) {
+    this.count--;
+    var temp = this.storage[this.head];
+    this.head++;
+    return temp;
+  }
+};
+
+queueMethods.size = function(){
+  return this.count;
+};
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,7 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newStack = Object().create(stackMethods);
+  var newStack = Object.create(stackMethods);
 
   newStack.count = 0;
   newStack.storage = {};
