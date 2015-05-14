8e39d0b98e07999bbafd92f2fc44e8106191b204
Billy Rukh <billyrukh@gmail.com>
2015-05-14 12:23:12 -0500

    Added comments to functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,23 +3,24 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var length = 0;
-  var head = 0, tail = 0;
+  var length = 0; // amount of items in queue
+  var head = 0; // position of first item in queue
+  var tail = 0; // next open position at end of queue
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-    storage[tail] = value;
-    length++;
-    tail++;
+    storage[tail] = value; // value is inserted at end of queue
+    length++; // size of queue increases
+    tail++; // tail increases to indicate next open slot
   };
 
   someInstance.dequeue = function(){
-    if (length > 0) {
-      var result = storage[head];
-      delete storage[head];
-      length--;
-      head++;
+    if (length > 0) { 
+      var result = storage[head]; 
+      delete storage[head]; // remove item from queue
+      length--; // size decreases because element is removed
+      head++; // head moves to next element in line
 
       return result;
     }
