7a8b782269cc9c5d7d63576422eebd3583b89ec4
Billy Rukh <billyrukh@gmail.com>
2015-05-16 09:58:37 -0500

    Completed tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+
+    list.head = list.head.next;
+
+    return temp;
   };
 
   list.contains = function(target){
+    var temp = list.head;
+
+    while (temp) {
+      if (temp.value === target){
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
   };
 
   return list;
