11a7a4ed032ebb7801d1aba8be3fcc470daacf0f
Billy Rukh <billyrukh@gmail.com>
2015-05-16 16:01:44 -0500

    Implemented insert, retrive, & remove. Do not have all tests passing yet.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,46 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var pair = [k,v];
+
+ // if storage i is undefined, 
+  if (this._storage.get(i) === undefined){ 
+  // make a bucket (array) there
+    this._storage.set(i, []);
+  // push [kv] into bucket
+    this._storage.get(i).push(pair);
+  }
+
+// if storage i has a bucket,
+  if (this._storage.get(i)) {
+  // push [kv] into bucket
+  this._storage.get(i).push(pair); 
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+ var i = getIndexBelowMaxForKey(k, this._limit);
 
+ var bucket = this._storage.get(i);
+
+ if (bucket !== undefined) {
+  for (var j = 0; j < bucket.length; j++) {
+    var pair = bucket[j];
+    if (pair[0] === k) {
+      return pair[1];
+    }
+  };
+  
+ }
+
+ 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
 
 };
 
